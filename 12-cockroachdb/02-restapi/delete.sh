9f43b2198c33d033433ac7fce153240223070459
MrZeLee <mrzelee404@gmail.com>
2025-09-24 12:43:58 +0100

    chore: remove unused helm charts and kustomization files
    The following files have been removed as they are no longer needed:
    - 10-json-server/Chart.yaml
    - 10-json-server/fleet.yaml
    - 10-json-server/templates/configmap.yaml
    - 10-json-server/templates/deploy.yaml
    - 10-json-server/templates/hpa.yaml
    - 10-json-server/templates/pvc.yaml
    - 12-cockroachdb/01-cockroachdb/client-secure.yaml
    - 12-cockroachdb/01-cockroachdb/fleet.yaml
    - 12-cockroachdb/01-cockroachdb/kustomization.yaml
    - 12-cockroachdb/01-cockroachdb/values.yaml
    - 12-cockroachdb/02-restapi/Chart.yaml
    - 12-cockroachdb/02-restapi/bandwidth_limit_testing.js
    - 12-cockroachdb/02-restapi/concurrency_limit_testing.js
    - 12-cockroachdb/02-restapi/data/.gitignore
    - 12-cockroachdb/02-restapi/delete.sh
    - 12-cockroachdb/02-restapi/docker-compose.yml
    - 12-cockroachdb/02-restapi/failure_injection_testing.js
    - 12-cockroachdb/02-restapi/fleet.yaml
    - 12-cockroachdb/02-restapi/shell.nix
    - 12-cockroachdb/02-restapi/soak_testing.js
    - 12-cockroachdb/02-restapi/templates/deploy.yaml
    - 12-cockroachdb/02-restapi/templates/hpa.yaml
    - 12-cockroachdb/02-restapi/templates/ingress-route.yaml
    - 12-cockroachdb/02-restapi/templates/middleware.yaml
    - 12-cockroachdb/02-restapi/templates/service.yaml
    
    These files were part of previous deployments or testing setups and are no longer required for the current project configuration. Removing them simplifies the repository and reduces potential confusion.

--- 12-cockroachdb/02-restapi/delete.sh
+++
@@ -1,7 +0,0 @@
-curl -s -H "Authorization: Basic $TOKEN" "$DB_URL/test" \
-  | jq '.[].id' \
-  | xargs -I _ curl -s -H "Authorization: Basic $TOKEN" $DB_URL/test/_ -X DELETE
-
-curl -s -H "Authorization: Basic $TOKEN" "$DB_URL/bandwidth_tests" \
-  | jq '.[].id' \
-  | xargs -I = curl -s -H "Authorization: Basic $TOKEN" $DB_URL/bandwidth_tests/= -X DELETE
