f0cc2bf9c569ff391a7da1795585ed2d8a497d17
MrZeLee <mrzelee404@gmail.com>
2025-09-24 12:17:54 +0100

    feat(secrets): add grafana rendering token secret and update kustomization
    This commit introduces a new sealed secret for the Grafana rendering token, ensuring secure storage of this sensitive information.
    
    Additionally, the `kustomization.yaml` file has been updated to include the newly added secret. This ensures that the Grafana rendering token secret is properly managed within the Kubernetes environment.

--- 02-secrets/install.sh
+++ 02-secrets/install.sh
@@ -4,10 +4,11 @@ find files -type f -name '*.yaml' |
 	while read -r file; do
 		namespace=$(basename "$(dirname "$file")")
 		name=$(basename "$file" .yaml)
+    echo $name
 		kubectl create secret generic "$name" --namespace="$namespace" --from-file=values.yaml="$file" --dry-run=client -o yaml >"secrets/${namespace}-${name}.yaml"
 	done
 find secrets -name '*.yaml' -type f -printf "%f\0" |
-	xargs --null -I {} sh -c 'kubeseal -f secrets/$1 -o yaml > seal-secrets/seal-$1' -- {}
+	xargs --null -I {} sh -c 'echo $1; kubeseal -f secrets/$1 -o yaml > seal-secrets/seal-$1' -- {}
 echo "resources:" >seal-secrets/kustomization.yaml
 find seal-secrets -name '*.yaml' -type f ! -name 'kustomization.yaml' -printf "%f\0" |
 	xargs --null -I {} sh -c 'echo "- $1" >> seal-secrets/kustomization.yaml' -- {}
