1bd334bd186be708870b1e3dbe1e057b551e906e
MrZeLee <mrzelee404@gmail.com>
2025-09-25 03:42:58 +0100

    feat(secrets): refactor secrets generation and management
    This commit introduces several changes to the secrets management process:
    
    - **New .gitignore for bundles:** A new `.gitignore` file is added to the `bundles` directory to prevent accidental commits of generated files.
    - **Removed unused dependency:** The `traefik-prep` dependency is removed from `fleet.yaml` as it is no longer needed.
    - **Updated install.sh:** The `install.sh` script is modified to:
        - Generate secrets for files within the `bundles` directory.
        - Create `kustomization.yaml` files for secrets within bundles.
    - **Updated sealed secrets:** The encrypted data for several sealed secrets has been updated. This is likely due to changes in the underlying values or re-encryption.
    - **Added Traefik secrets:** New files for Traefik secrets have been added, including:
        - `fleet.yaml` for Traefik secrets.
        - `helper-values-secret.yaml` for helper values.
        - `kustomization.yaml` for Traefik secrets.
        - `values-secret.yaml` for Traefik values.
    - **Updated Traefik fleet dependency:** The `dependsOn` section in the Traefik `fleet.yaml` is updated to reference `traefik-secrets` instead of `secrets`.
    
    These changes aim to improve the organization and automation of secret management, particularly for Traefik-related configurations.

--- 02-secrets/install.sh
+++ 02-secrets/install.sh
@@ -7,6 +7,22 @@ find files -type f -name '*.yaml' |
     echo $name
 		kubectl create secret generic "$name" --namespace="$namespace" --from-file=values.yaml="$file" --dry-run=client -o yaml >"secrets/${namespace}-${name}.yaml"
 	done
+find bundles -type f -name '*.yaml' |
+	while read -r file; do
+		path_dir=$(dirname "$file")
+    bundle_dir="${path_dir#bundles/}"
+		name=$(basename "$file" .yaml)
+    echo $name
+    kubectl create secret generic "$name" --from-file=values.yaml="$file" --dry-run=client -o yaml >"../${bundle_dir}/secrets/${name}.yaml"
+	done
+find bundles -mindepth 1 -type d |
+  while read -r file; do
+    bundle_dir="${file#bundles/}"
+    {
+      echo "resources:"
+      find "../${bundle_dir}/secrets" -name '*.yaml' -type f ! -name 'kustomization.yaml' ! -name 'fleet.yaml' -printf "- %f\n"
+    } | tee "../${bundle_dir}/secrets/kustomization.yaml" > /dev/null
+  done
 find secrets -name '*.yaml' -type f -printf "%f\0" |
 	xargs --null -I {} sh -c 'echo $1; kubeseal -f secrets/$1 -o yaml > seal-secrets/seal-$1' -- {}
 echo "resources:" >seal-secrets/kustomization.yaml
