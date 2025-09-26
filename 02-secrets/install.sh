35a6c07db24f06cd3814d2c114143fa969376822
MrZeLee <mrzelee404@gmail.com>
2025-09-26 23:12:20 +0100

    refactor(secrets): restructure secrets management and templates
    This commit refactors the secrets management and templating system.
    The primary goals are:
    - Consolidate secret templates into a more organized directory structure.
    - Remove unused and outdated secret templates.
    - Simplify the `install.sh` script by removing redundant logic and focusing on generating secrets from bundles.
    - Introduce `.gitignore` files to specific bundle directories to prevent accidental commits of generated secrets.
    - Add new secret templates for cert-manager, traefik, rancher, longhorn, and monitoring to support their specific configurations.
    - Update existing templates to use a more consistent and clear format.
    - Remove the `seal-secrets` kustomization as it's no longer needed with the new structure.
    - Remove the `.gitignore` from the `secrets` directory as it's now handled at the bundle level.

--- 02-secrets/install.sh
+++ 02-secrets/install.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-find files -type f -name '*.yaml' |
-	while read -r file; do
-		namespace=$(basename "$(dirname "$file")")
-		name=$(basename "$file" .yaml)
-    echo $name
-		kubectl create secret generic "$name" --namespace="$namespace" --from-file=values.yaml="$file" --dry-run=client -o yaml >"secrets/${namespace}-${name}.yaml"
-	done
 find bundles -type f -name '*.yaml' |
 	while read -r file; do
 		path_dir=$(dirname "$file")
@@ -32,8 +25,3 @@ find bundles -mindepth 2 -type d |
       find "../${bundle_dir}/secrets" -name '*.yaml' -type f ! -name 'kustomization.yaml' ! -name 'fleet.yaml' -printf "- %f\n"
     } | tee "../${bundle_dir}/secrets/kustomization.yaml" > /dev/null
   done
-find secrets -name '*.yaml' -type f -printf "%f\0" |
-	xargs --null -I {} sh -c 'echo $1; kubeseal -f secrets/$1 -o yaml > seal-secrets/seal-$1' -- {}
-echo "resources:" >seal-secrets/kustomization.yaml
-find seal-secrets -name '*.yaml' -type f ! -name 'kustomization.yaml' -printf "%f\0" |
-	xargs --null -I {} sh -c 'echo "- $1" >> seal-secrets/kustomization.yaml' -- {}
